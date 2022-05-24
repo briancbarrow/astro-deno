0d33fdcdd33623a2e43c484c395b72d7272ae199
Brian Barrow <briancbarrow@gmail.com>
2022-05-24 16:09:28 -0600

    fixed netlify cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import deno from "@astrojs/deno";
+import netlify from "@astrojs/netlify";
 
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
-  adapter: deno(),
+  adapter: netlify(),
 });
